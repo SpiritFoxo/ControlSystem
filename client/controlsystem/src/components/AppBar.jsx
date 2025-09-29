fce2288148a2d46b4476427c916e73221b33018e
Gleb Lobachev <gleblobachev@gmail.com>
2025-09-29 19:44:49 +0300

    feat redirect to main page by clicking logotype

--- client/controlsystem/src/components/AppBar.jsx
+++ client/controlsystem/src/components/AppBar.jsx
@@ -3,16 +3,22 @@ import Box from '@mui/material/Box';
 import Button from "@mui/material/Button";
 import Container from '@mui/material/Container';
 import Toolbar from '@mui/material/Toolbar';
+import { useNavigate } from 'react-router-dom';
 
 const Header = () => {
+    const nav = useNavigate();
+
+    const handleLogoClick = () => {
+        nav("/");
+    }
 
     return (
         <Box sx={{ flexGrow: 1, position: 'relative', zIndex: 999 }}>
             <AppBar position="static" sx={{ backgroundColor: '#ffffffff' }}>
                 <Container maxWidth="xl">
                     <Toolbar disableGutters>
-                        <Box component="img" src="/images/logotype-desktop.png" alt="Logo" sx={{ display: {xs: 'none', md: 'block'}}}></Box>
-                        <Box component="img" src="/images/logotype-mobile.png" alt="Logo" sx={{ display: {xs: 'block', md: 'none'}}}></Box>
+                        <Box onClick={handleLogoClick} component="img" src="/images/logotype-desktop.png" alt="Logo" sx={{ display: {xs: 'none', md: 'block'}}}></Box>
+                        <Box onClick={handleLogoClick} component="img" src="/images/logotype-mobile.png" alt="Logo" sx={{ display: {xs: 'block', md: 'none'}}}></Box>
                         
                         <Box sx={{ ml: 'auto', gap: {xs: 1, md: 2}, display: 'flex' }}>
                             <Button variant="contained" href="/admin">Админ-панель</Button>
